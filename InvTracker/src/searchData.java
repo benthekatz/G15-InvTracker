13345795d57e913654942aa9396bd395919cb06e
Zach <zzt5046@psu.edu>
2017-04-13 16:11:57 -0400

    Search function and window!

--- InvTracker/src/searchData.java
+++ InvTracker/src/searchData.java
@@ -1,11 +1,15 @@
 
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.util.ArrayList;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -21,25 +25,54 @@ import javax.swing.table.DefaultTableModel;
 public class searchData {
     
     ArrayList<InvObject> dataToSearch =  app.activeDB.getObjects();
-    ArrayList<InvObject> searchResults = new ArrayList<InvObject>();
-    
+    ArrayList<InvObject> searchResults = new ArrayList<InvObject>();  
     String giantResultsString;
+    
+    JFrame frame;
+    JTable table;
+    JPanel tablePanel;
+    String col[] = {"Name","Quantity","Notes","Date Added"};
     
     public searchData(String keyword){
         
+        fillSearchResults(keyword);
+                DefaultTableModel tableModel = new DefaultTableModel(col, 0);
+                table = new JTable(tableModel);
+                table.setSize(600,600);
+                table.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 16));
+                tablePanel = new JPanel();
+                tablePanel.add(table);
+                
+                String rowName;
+                String rowQuantity;
+                String rowNote;
+                String rowDate;
+                for(int i = 0; i < searchResults.size(); i++){
+                    
+                    rowName = searchResults.get(i).getObjectTitle();
+                    rowQuantity = searchResults.get(i).getQuantity();
+                    rowNote = searchResults.get(i).getNote();
+                    rowDate = searchResults.get(i).getDateLastUpdated();
+                    
+                    tableModel.addRow(new Object[]{rowName, rowQuantity, rowNote, rowDate});
+                }
+                
+                frame = new JFrame("Search Results");
+                frame.setSize(600,600);
+                frame.add(tablePanel);
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame.add(new JScrollPane(table));
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+       
+    }
+    public void fillSearchResults(String searchKey){
+        
         for(int i = 0; i < dataToSearch.size(); i++){
-            if(dataToSearch.get(i).getObjectTitle().contains(keyword)){
+            if(dataToSearch.get(i).getObjectTitle().contains(searchKey)){
                 searchResults.add(dataToSearch.get(i));
             }
         }
-        giantResultsString="";
-       for(int i = 0; i < searchResults.size(); i++){
-           if(searchResults.isEmpty()){
-               JOptionPane.showMessageDialog(null, "No results for: " + keyword);
-           }
-         giantResultsString = giantResultsString += "Name: " + searchResults.get(i).getObjectTitle() + " || Quantity: " + dataToSearch.get(i).getQuantity() + " || Notes: " + dataToSearch.get(i).getNote() + " || Date Added: " + dataToSearch.get(i).getDateLastUpdated() + "\n";
-       }
-       JOptionPane.showMessageDialog(null, giantResultsString);
-       
     }
 }
